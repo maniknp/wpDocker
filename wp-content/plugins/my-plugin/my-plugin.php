8eb091b9eb6d1931328c354fbeab2144b385f34b
chandramni <chandramani163585@gmail.com>
2024-05-25 00:00:13 +0530

    Updated: my-plugin.php

---
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Plugin Name: my-plugin
+ * Description: This is my first plugin.
+ * Version: 1.0.0
+ * Author: Surya
+ * Author URI: https://surya.com
+ * License: GPLv2 or later
+ * Text Domain: my-plugin
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
